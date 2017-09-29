22f3f460522cf73d52c0668e929baf7a616bd259
Jeevana Bommannagari <jeevana@bighead.net>
2017-09-28 22:51:29 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const https = require('https');
+
+
+function print(city,temp){
+  console.log(`Current temperature in ${city} is ${temp}F`);
+}
+
+function errorFunc(error){
+  console.log(`Error: ${error.message}`);
+}
+
+function get(zip){
+  try{
+      
+      https.get(`https://api.wunderground.com/api/58430949e99d6f7e/geolookup/conditions/q/${zip}.json`,response => {
+      let body = '';
+      
+      if(response.statusCode === 200){
+          response.on('data', (data) => {
+            body += data;
+          });
+          
+          response.on('end', () => {
+           const result = JSON.parse(body);
+           print(result.location.city,result.current_observation.temp_f);   
+          });
+        
+      }
+                
+      else {
+      console.error(`ERROR in fetching data, Status Code: ${response.statusCode}`);
+      }
+
+    }); 
+
+  } catch(error) { errorFunc(error); }
+}
+
+const zipcode = process.argv.slice(2);
+zipcode.forEach((zip) => { get(zip); })
